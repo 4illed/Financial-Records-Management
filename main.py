b35d0d6f67b7b33615ab0b7ee8d0c51941d1f369
BoychenkoKolya <fesetar@gmail.com>
2025-02-15 14:58:46 +0300

    Add new function edit record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from record.record_utils import RecordUtils
 from record.record import Record
-
+import csv
 
 def main():
 
@@ -61,8 +61,53 @@ def main():
                 print('Произошла ошибка при удалении')      
         elif choice == "3":
             # Редактировать запись
+            try:
+                record_id = input('Введите ID записи, которую хотите изменить: ')
+                file_path = input("Введите путь к файлу (по умолчанию records.csv): ")
+                if not file_path:
+                    file_path = "records.csv"
             
-            record_utils.edit_record()
+                with open(file_path, 'r') as file:
+                    lines = list(csv.reader(file))
+                found = False
+                for i, line in enumerate(lines):
+                    if line[0] == record_id:
+                        found = True
+                        print(f"Ваша текущая запись {record_id}")
+                        break
+                if not found:
+                    print(f'Произошла ошибка. Запись в ID {record_id} не была найдена')
+                    continue
+                
+                while True:
+                    record_type = input(f"Введите тип записи (income/expense{line[2]}): ").strip().lower()
+                    if record_type not in ['income', 'expense']:
+                        print('Ошибка. Тип записи должен быть "income" или "expense".')
+                        continue
+                    break
+                
+                category = input(f"Введите новую категорию ({line[3]}): ").strip()
+                
+                description = input(f"Введите новое описание ({line[4]}): ").strip()
+                
+                while True:
+                    try:
+                        amount = float(input(f'Введите сумму ({line[5]}): '))
+                        if amount > 0 and record_type == 'income':
+                            amount = -amount
+                        elif amount < 0 and record_type == 'expense':
+                            amount = abs(amount)
+                        break
+                    except ValueError:
+                        print('Ошибка. Ввод должен быть числом')
+                        
+                updated_record = Record(record_type, category, amount, description)
+                updated_record.id = record_id
+                updated_record.date = line[1]
+                record_utils.edit_record(record_id=record_id, updated_record=updated_record, file_path=file_path)
+                print('Запись успешно изменена')
+            except:
+                print('Произошла ошибка при изменении')
         elif choice == "4":
             # Поиск записей
             record_utils.search_records()
