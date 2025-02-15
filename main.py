7f8caa3610092673a6907d09b25fe3bd7d571741
BoychenkoKolya <fesetar@gmail.com>
2025-02-15 14:33:01 +0300

    Add new functions add and delete

--- main.py
+++ main.py
@@ -22,22 +22,46 @@ def main():
         # Обработать выбор пользователя
         if choice == "1":
             # Добавить запись
-            new_record = Record(
-                input("Введите тип записи (income/expense): "),
-                input("Введите категорию: "),
-                # todo: добавить проверку на ввод числа
-                float(input("Введите сумму: ")),
-                input("Введите описание (необязательно): "),
-            )
+            while True:
+                record_type = input("Введите тип записи (income/expense): ").strip().lower()
+                if record_type not in ['income', 'expense']:
+                    print('Ошибка. Тип записи должен быть "income" или "expense".')
+                    continue
+                break
+            category = input('Введите категорию: ')
+            
+            while True:
+                try:
+                    amount = float(input('Введите сумму: '))
+                    if amount > 0 and record_type == 'income':
+                        amount = -amount
+                    elif amount < 0 and record_type == 'expense':
+                        amount = abs(amount)
+                    break
+                except ValueError:
+                    print('Ошибка. Ввод должен быть числом')
+            
+            description = input('Введите описание: ')
+            # Создаем новую запись  
+            new_record = Record(record_type, category, amount, description)
             file_path = input("Введите путь к файлу (по умолчанию records.csv): ")
             if not file_path:
                 file_path = "records.csv"
-            record_utils.add_record(record=new_record, file_path=file_path)
+            record_utils.add_record(record=new_record, file_path=file_path)     
         elif choice == "2":
             # Удалить запись
-            record_utils.delete_record()
+            try:
+                record_id = input('Введите ID записи, которую хотите удалить: ')
+                file_path = input("Введите путь к файлу (по умолчанию records.csv): ")
+                if not file_path:
+                    file_path = "records.csv"
+                record_utils.delete_record(record_id, file_path)
+                print('Запись успешно удалена')
+            except:
+                print('Произошла ошибка при удалении')      
         elif choice == "3":
             # Редактировать запись
+            
             record_utils.edit_record()
         elif choice == "4":
             # Поиск записей
